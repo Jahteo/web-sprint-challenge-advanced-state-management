5dd3760681ca1b70afe351d47efe69107514bd50
Josh Glantz-Hucks <jahteo@gmail.com>
2020-09-11 08:21:54 -0700

    installed dependencies, begin setting up store

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const reducer = () => {};
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(<Provider store ={store}><App /></Provider>, document.getElementById("root"));
